f8158c4da5cb04ac0e3f8b3102607ed505abe627
Patipan <patipan.bo@ku.th>
2021-07-23 16:27:37 +0700

    add ledger page, delete About page

---
+++ src/views/Ledger.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Ledger Assignment</h1>
+    <ledger-form></ledger-form>
+    <ledger-table></ledger-table>
+  </div>
+</template>
+
+<script>
+import LedgerForm from '../components/LedgerForm.vue'
+import LedgerTable from '@/components/LedgerTable'
+
+export default {
+  components: { 
+    LedgerForm,
+    LedgerTable
+  }
+
+}
+</script>
+
+<style>
+
+</style>
