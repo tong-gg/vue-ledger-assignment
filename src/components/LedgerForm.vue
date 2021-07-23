bf756bf8d62e3d3e908591710b43612bd679ef88
Patipan <patipan.bo@ku.th>
2021-07-23 16:27:05 +0700

    ledger components

---
+++ src/components/LedgerForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="input-elem">
+      <label>เลือกวันที่ </label>
+      <input type="date">
+    </div>
+    
+    <div class="input-elem" id="inc-exp">
+      <label>เลือกรายรับ/รายจ่าย </label>
+      <select>
+        <option value="income">รายรับ(Income)</option>
+        <option value="expense">รายจ่าย(Expense)</option>
+      </select>
+    </div>
+
+    <div class="input-elem" id="baht">
+      <label>จำนวนเงิน </label>
+      <input type="text">
+      <label> บาท</label>
+    </div>
+
+    <div class="input-elem" id="note-div">
+      <label id="note-label">Note </label>
+      <textarea id="note-textfield" rows="6" cols="50"></textarea>
+    </div>
+
+    <button>บันทึก</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .input-elem {
+    margin: 1em;
+  }
+
+  textarea {
+    resize: none;
+  }
+  
+</style>
---
+++ src/components/LedgerTable.vue
@@ -0,0 +1,77 @@
+<template>
+    <div style="margin-top: 3em"> 
+        <input type="date">
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>ประเภท</th>
+                    <th>รายละเอียด</th>
+                    <th>จำนวนเงิน</th>
+                    <th>วันที่บันทึก</th>
+                </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in data.detail" :key="index">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ item.type }}</td>
+                  <td>{{ item.note }}</td>
+                  <td>{{ item.price }}</td>
+                  <td>{{ data.date }}</td>
+                  <td>
+                      <button>edit</button>
+                  </td>
+                  <td>
+                      <button>delete</button>
+                  </td>
+              </tr>
+          </tbody>
+        </table>
+          <p>ยอดรวมรายรับ: 1000</p>
+          <p>ยอดรวมรายจ่าย: 1500</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            data: {
+                    date: Date.now(),
+                    detail: [
+                        {
+                            type: 'รายรับ',
+                            note: 'คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้',
+                            price: 1000,
+                        },
+                        {
+                            type: 'รายรับ',
+                            note: 'แม่จ้างล้างจาน',
+                            price: 100,
+                        },
+                        {
+                            type: 'รายจ่าย',
+                            note: 'ซื้อข้าว',
+                            price: 100,
+                        },
+                        {
+                            type: 'รายจ่าย',
+                            note: 'ซื้อสกินแคร์',
+                            price: 500,
+                        },
+                    ],
+                    totalIncome: 1100,
+                    totalExpense: 600,
+                    balance: 500
+                },
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    table {
+        table-layout: fixed;
+        width: 100%
+    }
+</style>
