366058d5c02dc6103dfd832509c44e5ef951c9c3
Patipan <patipan.bo@ku.th>
2021-07-26 03:21:02 +0700

    ledger component, all requirements spec checked!

--- src/components/LedgerForm.vue
+++ src/components/LedgerForm.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
+  <div class="container mx-auto my-16 text-xl">
+    <!-- TODO: validation -->
     <div class="input-elem">
       <label>เลือกวันที่ </label>
-      <input type="date">
+      <input type="date" v-model="form.date" id="dateForm" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
     </div>
     
     <div class="input-elem" id="inc-exp">
       <label>เลือกรายรับ/รายจ่าย </label>
-      <select>
+      <select v-model="form.detail.type" class="border border-gray-300 shadow rounded text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none">
+        <option value="" selected disabled>เลือก option</option>
         <option value="income">รายรับ(Income)</option>
         <option value="expense">รายจ่าย(Expense)</option>
       </select>
@@ -15,22 +17,87 @@
 
     <div class="input-elem" id="baht">
       <label>จำนวนเงิน </label>
-      <input type="text">
+      <input type="text" v-model="form.detail.price" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
       <label> บาท</label>
     </div>
 
+    <label id="note-label">Note</label>
     <div class="input-elem" id="note-div">
-      <label id="note-label">Note </label>
-      <textarea id="note-textfield" rows="6" cols="50"></textarea>
+      <textarea id="note-textfield" rows="6" cols="50" v-model="form.detail.note" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
     </div>
-
-    <button>บันทึก</button>
+    <div class="flex justify-center">
+      <button @click="addNewLedgerList" class="w-1/5 px-6 py-3 mb-4 text-base font-semibold leading-6 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200 rounded-full block  border-b border-purple-300 bg-blue-300 hover:bg-blue-500 text-blue-900">
+      บันทึก
+      <font-awesome-icon icon="save"/>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import LedgerStore from '@/store/LedgerStore'
+import LedgerTable from '@/components/LedgerTable'
 
+export default {
+  components: {
+    LedgerTable
+  },
+  data() {
+    return {
+      form: {
+        date: '',
+        detail: {
+          type: '',
+          note: '',
+          price: ''
+        },
+      }
+    }
+  },
+  mounted() {
+    let date = this.$el.querySelector("#dateForm")
+    let today = new Date().toISOString().substr(0, 10);
+    date.value = today
+    this.form.date = today
+  },
+  methods: {
+    addNewLedgerList() {
+      let price = parseInt(this.form.detail.price)
+      let detail = {
+        type: this.form.detail.type,
+        note: this.form.detail.note,
+        price: price
+      }
+      let payload = {
+        date: this.form.date,
+        detail: detail,
+      }
+
+      if (this.form.detail.type === "income"){
+        payload.detail.type = "รายรับ"
+      } else {
+        payload.detail.type = "รายจ่าย"
+      }
+
+      LedgerStore.dispatch('addLedgerList', payload)
+      console.log(payload)
+      this.clearForm()
+    },
+    clearForm() {
+      this.form = {
+            date: '',
+            detail: {
+              type: '',
+              note: '',
+              price: ''
+          }
+        }
+    let date = this.$el.querySelector("#dateForm")
+    let today = new Date().toISOString().substr(0, 10);
+    date.value = today
+    this.form.date = today
+      }
+    }
 }
 </script>
 
@@ -41,6 +108,10 @@ export default {
 
   textarea {
     resize: none;
+  }
+
+  select option[disabled]:first-child {
+	  display: none;
   }
   
 </style>
--- src/components/LedgerTable.vue
+++ src/components/LedgerTable.vue
@@ -1,77 +1,190 @@
 <template>
     <div style="margin-top: 3em"> 
-        <input type="date">
-        <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>ประเภท</th>
-                    <th>รายละเอียด</th>
-                    <th>จำนวนเงิน</th>
-                    <th>วันที่บันทึก</th>
+        <div class="container mx-auto">
+            <div class="flex space-x-4 items-baseline">
+                <input type="date" id="dateTable" v-model="today" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                <button @click="searchByDate" class="w-auto px-6 py-3   mb-4 text-base  font-semibold leading-6 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200 rounded-full block  border-b border-purple-300 bg-blue-300 hover:bg-blue-500 text-blue-900">
+                    ค้นหา
+                     <font-awesome-icon icon="search"/>
+                </button>  
+                <div v-if="data !== undefined" class="flex space-x-8 justify-center text-2xl" style="margin-left: auto">
+                <div class="text-white rounded-2xl px-4 py-4" style="background-color: #5C969E">
+                <p>ยอดรวมรายรับรายวัน</p>
+                <p>{{ data.totalIncome }} บาท</p>
+                </div>
+                
+                <div class="text-white rounded-2xl px-4 py-4" style="background-color: #F38BA0">
+                <p>ยอดรวมรายจ่ายรายวัน</p>
+                <p>{{ data.totalExpense }} บาท</p>
+                </div>
+
+                <div class="text-white rounded-2xl px-4 py-4" style="background-color: #CFC5A5">
+                <p>ยอดคงเหลือ </p>
+                <p>{{ data.balance }} บาท</p>
+                </div>
+        </div>
+        </div>
+        </div>
+        <div v-if="data !== undefined && data.balance !== 0" class="container mx-auto">
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+            <table class="min-w-full leading-normal text-2xl">
+                <thead>
+                    <tr>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-lg font-semibold text-gray-600 uppercase tracking-wider">#</th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-lg font-semibold text-gray-600 uppercase tracking-wider">ประเภท</th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-lg font-semibold text-gray-600 uppercase tracking-wider">รายละเอียด</th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-lg font-semibold text-gray-600 uppercase tracking-wider">จำนวนเงิน</th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-lg font-semibold text-gray-600 uppercase tracking-wider">วันที่บันทึก</th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-lg font-semibold text-gray-600 uppercase tracking-wider"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(item, index) in data.detail" :key="index" class="bg-gray-300">
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-lg">{{ index + 1 }}</td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-lg">
+                       <span v-if="item.type === `รายรับ`" class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                            <span v-if="item.type === `รายรับ`" aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                <span class="relative">{{item.type}}</span>
+                        </span>
+                        <span v-if="item.type === `รายจ่าย`" class="relative inline-block px-3 py-1 font-semibold text-yellow-900 leading-tight">
+                            <span v-if="item.type === `รายจ่าย`" aria-hidden class="absolute inset-0 bg-yellow-200 opacity-50 rounded-full"></span>
+                            <span class="relative">{{item.type}}</span>
+                        </span>
+                    </td>
+
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-lg">
+                        {{ item.note }}
+                    </td>
+
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-lg">{{ item.price }}</td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-lg">{{ data.date }}</td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-lg">
+                        <button @click="deleteItem(index)" class="w-auto px-3 py-1 leading-6 font-semibold focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200 rounded border-b border-purple-300 bg-red-300 hover:bg-red-500 text-red-900">
+                            <font-awesome-icon icon="times" />
+                        </button>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in data.detail" :key="index">
-                  <td>{{ index + 1 }}</td>
-                  <td>{{ item.type }}</td>
-                  <td>{{ item.note }}</td>
-                  <td>{{ item.price }}</td>
-                  <td>{{ data.date }}</td>
-                  <td>
-                      <button>edit</button>
-                  </td>
-                  <td>
-                      <button>delete</button>
-                  </td>
-              </tr>
-          </tbody>
-        </table>
-          <p>ยอดรวมรายรับ: 1000</p>
-          <p>ยอดรวมรายจ่าย: 1500</p>
+            </tbody>
+            </table>
+                </div>
+            </div>
+        </div>
+        <div v-if="data === undefined || data.balance === 0" class="mt-28">
+            <h3 class="text-3xl">ว้าาา วันนี้ยังไม่ได้ใช้จ่ายอะไรเลย</h3>
+        </div>
+        <div class="flex space-x-8 justify-center text-2xl my-16" style="margin-left: auto">
+                <div class="text-white tracking-wider rounded-2xl px-4 py-4" style="background-color: #628395">
+                <p>ยอดรวมรายรับ</p>
+                <p>{{ sum.income }} บาท</p>
+                </div>
+
+                <div class="text-white tracking-wider rounded-2xl px-4 py-4" style="background-color: #FFA0A0">
+                <p>ยอดรวมรายจ่าย</p>
+                <p>{{ sum.expense }} บาท</p>
+            </div>
+       
+         <div v-if="data === undefined" class="flex space-x-8 justify-center my-16 text-2xl" style="margin-left: auto">
+                <div class="text-white rounded-2xl px-4 py-4" style="background-color: #5C969E">
+                <p>ยอดรวมรายรับรายวัน</p>
+                <p>0 บาท</p>
+                </div>
+                
+                <div class="text-white rounded-2xl px-4 py-4" style="background-color: #F38BA0">
+                <p>ยอดรวมรายจ่ายรายวัน</p>
+                <p>0 บาท</p>
+                </div>
+
+                <div class="text-white rounded-2xl px-4 py-4" style="background-color: #CFC5A5">
+                <p>ยอดคงเหลือ </p>
+                <p>0 บาท</p>
+                </div>
+        </div>
+        </div>
+        <div class="container mx-auto">
+            <ledger-chart v-bind:sum="sum"></ledger-chart>
+        </div>
     </div>
 </template>
 
 <script>
+import LedgerStore from '@/store/LedgerStore'
+import LedgerChart from '@/components/LedgerChart'
+
 export default {
+    components: {
+        LedgerChart
+    },
     data() {
         return {
-            data: {
-                    date: Date.now(),
-                    detail: [
-                        {
-                            type: 'รายรับ',
-                            note: 'คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้คุณแม่ให้',
-                            price: 1000,
-                        },
-                        {
-                            type: 'รายรับ',
-                            note: 'แม่จ้างล้างจาน',
-                            price: 100,
-                        },
-                        {
-                            type: 'รายจ่าย',
-                            note: 'ซื้อข้าว',
-                            price: 100,
-                        },
-                        {
-                            type: 'รายจ่าย',
-                            note: 'ซื้อสกินแคร์',
-                            price: 500,
-                        },
-                    ],
-                    totalIncome: 1100,
-                    totalExpense: 600,
-                    balance: 500
+            data: {},
+            today: '',
+            editIndex: -1,
+            form: {
+                date: '',
+                detail: {
+                    type: '',
+                    note: '',
+                    price: ''
                 },
+            },
+            sum: 0
+        }
+    },
+    mounted() {
+        let dateTable = this.$el.querySelector("#dateTable")
+        let today = new Date().toISOString().substr(0, 10);
+        dateTable.value = today
+        this.today = today
+    },
+    async created() {
+        let today = new Date().toISOString().substr(0, 10);
+        await this.fetchLedgerList(today)
+        // bug fixed!
+        this.sum = LedgerStore.getters.getSum
+        LedgerStore.watch(
+            (state) => {
+                return LedgerStore.getters.ledgerLists
+            }, 
+            (newValue, oldValue) => {
+                this.data = newValue
+                console.log(this.data.detail)
+            }
+        )
+        LedgerStore.watch(
+            (state) => {
+                return LedgerStore.getters.getSum
+            }, 
+            (newValue, oldValue) => {
+                this.sum = newValue
+            }
+        )
+    },
+    methods: {
+        async fetchLedgerList(date) {
+            console.log('fired!')
+            // invoke action method using dispatch
+            await LedgerStore.dispatch('fetchData', date)
+            
+            // filter แล้ว reference เปลี่ยน
+            // ลองเอาไปฟิลเตอร์ใน store สิ
+            this.data = LedgerStore.getters.ledgerLists
+            console.log(this.data)
+        },
+        async searchByDate() {
+            console.log('fired!')
+            console.log(this.today);
+            await LedgerStore.dispatch('searchLedger', this.today)
+            this.data = LedgerStore.getters.ledgerLists
+            console.log(this.data)
+        },
+        deleteItem(index) {
+            LedgerStore.dispatch('deleteItem', index)
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    table {
-        table-layout: fixed;
-        width: 100%
-    }
+
 </style>
