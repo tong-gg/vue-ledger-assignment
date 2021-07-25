0d22c6294aef88a8282341ab29a990ed060d53c9
Patipan <patipan.bo@ku.th>
2021-07-26 03:21:40 +0700

    import tailwind, fontawesome

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/tailwind.css'
+import '@/plugins/apexcharts'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faSave, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch, faSave, faTimes)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
