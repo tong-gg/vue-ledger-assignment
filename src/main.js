b4bfef1aa06f3f323b446f149cb399c175259f8d
Patipan <patipan.bo@ku.th>
2021-07-26 14:45:13 +0700

    import ledgerstore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import LedgerStore from '@/store/LedgerStore'
 import '@/assets/tailwind.css'
 import '@/plugins/apexcharts'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -16,6 +16,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  LedgerStore,
   render: h => h(App)
 }).$mount('#app')
